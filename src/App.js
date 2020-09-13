a2c3114854fdee5cd8ad761e3eaad5e371aaa2b2
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-09-14 01:00:35 +0545

    useState increment button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
     <div className="App">
-      <h2>React Hooks</h2>
+      <h2>React Hooks - Increment</h2>
+      <h1>{value}</h1>
+      <button onClick={() => setValue(value + 1)}>Increment</button>
     </div>
   );
 }
